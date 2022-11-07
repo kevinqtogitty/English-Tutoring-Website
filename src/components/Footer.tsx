5b8815fc2bb84062e7662c19cef10a01f4e9731c
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-07 20:55:59 +0100

    Changed z index so footer sits on top of bg image

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className=" w-full h-20 flex justify-center items-center text-slate-50 gap-5 z-10">
+      <p>Site built by Kevin To</p>
+      <p>Copyright © 2022 Kevin To Tutoring</p>
+      <p>All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
