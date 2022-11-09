be377b4836196f404be148dae35e1a332d681cd7
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-09 20:39:45 +0100

    Added breakpoints

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,23 +12,27 @@ const Home: React.FC<HomeProps> = (props) => {
   return (
     <section
       id="top"
-      className=" h-screen flex justify-center align-top font-roboto relative w-[99rem]"
+      className=" h-screen flex justify-center align-top font-roboto relative w-screen sm:w-[86rem] mb-10"
       ref={props.homeRef}
     >
       {' '}
-      <article className=" w-full h-fit p-2 flex justify-around absolute top-44">
-        <img src={profilePhoto} alt="" className="h-96 rounded-full" />
-        <div className=" w-fit flex flex-col items-end gap-2">
-          <h1 className="text-xl5 text-right">
+      <article className=" sm:w-full h-fit gap-5 p-2 flex flex-col sm:flex-row justify-around absolute top-24 sm:top-44 ">
+        <img
+          src={profilePhoto}
+          alt=""
+          className=" h-48 sm:h-96 rounded-full self-center"
+        />
+        <div className=" w-fit flex flex-col items-center sm:gap-2 text-center sm:text-right">
+          <h1 className=" text-xl1 sm:text-xl4 sm:text-right">
             Are you ready to take your
             <br />
-            <span className="relative after:z-[-1] after:content=[''] after:w-full after:border-solid after:border-b-[1.5rem] after:border-orange-500 after:absolute after:bottom-[-.05rem] after:left-0">
+            <span className="relative after:z-[-1] after:content=[''] after:w-full after:border-solid after:border-b-[.8rem] sm:after:border-b-[1.5rem] after:border-orange-500 after:absolute after:bottom-[-.05rem] after:left-0">
               English
             </span>{' '}
             to the next level?
           </h1>
           <br />
-          <h2 className="text-xl3 text-stone-500  flex justify-end text-right">
+          <h2 className="sm:text-xl2 text-stone-500  flex justify-end">
             Hi I'm Kevin! 🇺🇸 Your native speaking
             <br />
             English teacher here to help
@@ -40,8 +44,8 @@ const Home: React.FC<HomeProps> = (props) => {
             offset={0}
             duration={500}
           >
-            <button className=" group flex items-center gap-1 self-center mt-10 py-2 px-5 rounded-3xl bg-blue-500 text-white focus:ring-2 text-xl1 border-none">
-              Book a free 30min introduction lesson!
+            <button className=" group flex items-center gap-1 self-center mt-5 sm:mt-10 py-2 px-5 rounded-2xl bg-blue-500 text-white focus:ring-2 text-md border-none">
+              Book a free lesson!
               <img
                 src={dblArrow}
                 alt="double arrow right"
@@ -52,14 +56,16 @@ const Home: React.FC<HomeProps> = (props) => {
           </Link>
         </div>
       </article>
+      <h2 className="absolute bottom-64 text-xl1 sm:hidden">Reviews</h2>
       <Carousel
+        withIndicators
         slideSize="50%"
-        height={200}
-        slideGap="lg"
-        controlsOffset="lg"
+        height={220}
+        slideGap="md"
+        controlsOffset="md"
         controlSize={50}
         loop
-        className="absolute bottom-10 w-[90%]"
+        className="absolute bottom-16 sm:bottom-0 w-[80%]"
       >
         <Carousel.Slide>
           <SpeechBubble />
