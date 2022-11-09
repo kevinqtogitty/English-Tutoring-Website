f3329bdfd2df4fedb1911073cabbf242e05d2aad
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-09 20:40:08 +0100

    Added breakpoints

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,15 +11,19 @@ const About: React.FC<AboutProps> = (props) => {
   return (
     <div
       id="aboutSection"
-      className=" h-screen w-[99rem] flex justify-around items-center px-10 "
+      className=" sm:h-screen flex flex-col sm:flex-row justify-between items-center px-10 gap-18"
       ref={props.aboutRef}
     >
-      <div className="w-[40rem] flex flex-col items-start">
-        <img src={FriendsPicture} alt="" className="h-[30rem] self-center" />
-        <h2 className="text-[3rem] relative after:contents-[' '] after:absolute after:w-[95%] after:border-solid after:border-b-[2rem] after:bottom-[.6rem] after:left-5 after:border-yellow-300 after:z-[-1]">
+      <div className="sm:w-[40rem] flex flex-col sm:items-start items-center mb-8 sm:mb-0">
+        <img
+          src={FriendsPicture}
+          alt=""
+          className="hidden sm:block h-[25rem] self-center"
+        />
+        <h2 className="text-[2.3rem] relative after:contents-[' '] after:absolute after:w-[95%] after:border-solid after:border-b-[2rem] after:bottom-[.6rem] after:left-5 after:border-yellow-300 after:z-[-1]">
           Let's work together!
         </h2>
-        <p className="text-xl2 leading-7 w-[35rem] ml-8 relative after:contents-[' '] after:absolute after:w-[90%] after:border-solid after:border-b-[6.8rem] after:bottom-[-.8rem] after:right-20 after:border-blue-100 after:z-[-1]">
+        <p className=" sm:text-xl1 leading-7 w-[18rem] sm:w-[30rem] ml-8 relative after:contents-[' '] after:absolute after:w-[90%] after:border-solid after:border-b-[6.8rem] after:bottom-[.2rem] after:right-12 sm:after:bottom-[-.8rem] sm:after:right-20 after:border-blue-100 after:z-[-1]">
           I'm a TEFL and CELTA certified teacher, and have been teaching
           students professionally from all around the world for the past 3
           years. Here's what you can expect working with me :)
