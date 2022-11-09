d4b480c1582acf4bfb15ddc97b2630b090c66918
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-09 20:40:35 +0100

    Added breakpoints

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className=" w-full h-20 flex justify-center items-center text-slate-50 gap-5 z-10">
+    <footer className="w-[23rem] text-[.5rem] sm:text-sm sm:w-full sm:h-20 flex justify-center items-center text-slate-50 gap-5 z-10">
       <p>Site built by Kevin To</p>
       <p>Copyright © 2022 Kevin To Tutoring</p>
       <p>All rights reserved</p>
