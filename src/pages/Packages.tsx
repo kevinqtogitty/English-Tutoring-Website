c9d75d162482ff1a524522e3bc7f77a938369d7b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-18 13:53:41 +0100

    -

--- src/pages/Packages.tsx
+++ src/pages/Packages.tsx
@@ -7,9 +7,9 @@ interface PackagesProps {
 
 const Packages: React.FC<PackagesProps> = (props) => {
   const data = [
-    { lessons: 15, hourly: 140, discount: 0.1 },
-    { lessons: 20, hourly: 140, discount: 0.15 },
-    { lessons: 30, hourly: 140, discount: 0.2 }
+    { lessons: 15, hourly: 120, discount: 0.1 },
+    { lessons: 20, hourly: 120, discount: 0.15 },
+    { lessons: 30, hourly: 120, discount: 0.2 }
   ];
 
   return (
@@ -21,7 +21,7 @@ const Packages: React.FC<PackagesProps> = (props) => {
       <article className="text-xl3 flex flex-col items-center mt-10">
         <h2 className="sm:text-xl4">Hourly rate & packages</h2>
         <p className="text-[1.3rem] sm:text-[1.5rem] sm:text-md">
-          140 PLN/60mins & 200 PLN/90mins
+          120 PLN/60mins & 160 PLN/90mins
         </p>
       </article>
       <div className="flex sm:flex-row flex-col w-fit justify-around gap-6 sm:gap-20">
